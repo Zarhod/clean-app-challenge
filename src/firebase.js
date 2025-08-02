739f3ac788e14d31d8fe97e7bedd89a10bc6f6c9
Jasmin JB Bremond <33281492+Zarhod@users.noreply.github.com>
2025-08-02 20:32:41 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+export const db = getFirestore(app); // ✅ ajout important
